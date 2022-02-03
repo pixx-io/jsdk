f978c2074879e858e224aad86a2ecdd6d654295e
Robert Schwarzenböck <rs@pixx.io>
2022-02-03 12:40:16 +0100

    Improvement Upload: uploads are synchronous now => we wait for a finishedJob-websocket-event before we finish the function

--- src/Upload.svelte
+++ src/Upload.svelte
@@ -5,7 +5,7 @@
   import { createEventDispatcher } from "svelte";
   import Error from './Error.svelte';
   import { lang } from './translation';
-  import { modal } from './store/store';
+  import { modal, domain, accessToken } from './store/store';
   const api = new API();
   let loading = false;
   export let config = {};
@@ -21,8 +21,44 @@
     try {
       errorMessage = '';
       successMessage = '';
+
+      let uploadJobID = null;
+      let finishedWebsocketEvents = [];
+      const websocketUrl = 'wss://' + $domain.replace(/^(http|https):\/\//, '') + '/gobackend/ws?accessToken=' + $accessToken;
+      const socket = new WebSocket(websocketUrl);
+
+      const onFoundWebsocketEvent = (returnData) => {
+        socket.close();
+        uploadJobID = null;
+        finishedWebsocketEvents = [];
+
+        dispatch('uploaded', returnData);
+        return returnData;
+      };
+
+      socket.onmessage = (event) => {
+        try {
+          const lines = event.data.split("\n");
+          lines.forEach(line => {
+            let eventData = JSON.parse(line);
+            if (eventData.type === 'finishedJob') {
+              finishedWebsocketEvents.push(eventData);
+
+              if (uploadJobID && eventData.jobID === uploadJobID) {
+                const returnData = {
+                  success: true,
+                  id: eventData.jobData.fileID,
+                  isDuplicate: eventData.jobData.isDuplicate || false
+                };
+                onFoundWebsocketEvent(returnData);
+              }
+            }
+          });
+        } catch (e) { }
+      };
+
       const formData = new FormData();
-      formData.set('asynchronousConversion', false);
+      formData.set('asynchronousConversion', true);
       Object.keys(config).forEach(key => {
         if (key !== 'file') {
           formData.set(key, JSON.stringify(config[key]));
@@ -32,14 +68,30 @@
       })
       const response = await api.post('/files', formData, true, null, false, false);
       loading = false;
-      dispatch('uploaded', response);
       if (response.success && response.isDuplicate) {
         errorMessage = lang('duplicate_file');
         dispatch('error', lang('duplicate_file'));
       } else {
         successMessage = lang('success_upload_file');
       }
-      return response;
+
+      if (response.jobID) {
+        uploadJobID = response.jobID;
+        const foundWebsocketEvent = finishedWebsocketEvents.find((finishedEvent) => finishedEvent.jobID === uploadJobID);
+        if (foundWebsocketEvent) {
+          const returnData = {
+            success: true,
+            id: foundWebsocketEvent.jobData.fileID,
+            isDuplicate: foundWebsocketEvent.jobData.isDuplicate || false
+          };
+          onFoundWebsocketEvent(returnData);
+        }
+      }
+
+      if (!response.success) {
+        dispatch('uploaded', response);
+        return response;
+      }
     } catch(error) {
       loading = false; 
       errorMessage = error;
